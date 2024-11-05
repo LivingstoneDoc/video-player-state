6ccaf4ae2b48d641b931b20bbce364028db5f17a
LivingstoneDoc <livingstone.doc@yandex.ru>
2024-11-06 01:00:39 +0400

    feat: add modal window

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from 'react';
+import { Button, Modal } from 'antd';
 
 function App() {
+  const videoRef = useRef(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function openModal() {
+    setIsModalOpen(true);
+  }
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='player'>
+        <Button className='player_btn' onClick={openModal}>
+          <span className='player_btn__firstLine line'></span>
+          <span className='player_btn__secondLine line'></span>
+          <span className='player_btn__thirdLine line'></span>
+        </Button>
+        <Modal className='player_modal' title="Player" footer={null} open={isModalOpen} onCancel={closeModal} width={1000} height={700}>
+          <video className='player_modal__video' ref={videoRef} src='https://www.youtube.com/watch?v=22QAcwuGqFs'>
+            {/* <source src='https://www.youtube.com/watch?v=22QAcwuGqFs' type='video/mp4' /> */}
+          </video>
+        </Modal>
+      </div>
+      
     </div>
   );
 }
